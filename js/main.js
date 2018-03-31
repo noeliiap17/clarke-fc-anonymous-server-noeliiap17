9f316d7d73d9b70d9a0128a0162078514b7ade87
noeliiap17 <n.parra2003@gmail.com>
2018-03-31 12:17:11 +0200

    add animation to menu and more styles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var navMenu = document.querySelector('.nav');
+var btnMenu = document.querySelector('.nav-trigger');
 
 function showMenu() {
   navMenu.classList.remove('nav-hidden');
+  btnMenu.classList.add('btn-hidden');
 }
 function hiddenMenu() {
   navMenu.classList.add('nav-hidden');
+  btnMenu.classList.remove('btn-hidden');
+
 }
 var buttonNav = document.querySelector('.nav-trigger');
 var buttonClose = document.querySelector('.nav-close');
